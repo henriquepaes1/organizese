43f2a4dcafdd489defa8cf524d66e637d2a4da65
henriquepaes1 <81305601+henriquepaes1@users.noreply.github.com>
2022-02-24 15:22:12 -0300

    concluded database comments

--- conclude.py
+++ conclude.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-df_conc = sqlite3.connect('concluded.db')
+df_conc = sqlite3.connect('concluded.db') # initializes new db
 cursor_conclude = df_conc.cursor()
 
-cursor_conclude.execute("CREATE TABLE concluded(conc_task_id INTEGER, user_id INTEGER, desc TEXT, PRIMARY KEY (conc_task_id))")
+cursor_conclude.execute("CREATE TABLE concluded(conc_task_id INTEGER, user_id INTEGER, desc TEXT, PRIMARY KEY (conc_task_id))") # new db for concluded tasks
